b50a060413b9493e63e6a7f97e8cac03bf9fd427
flagrans <flagrans@mail.ru>
2022-08-29 02:01:53 +0700

    Homework #6

---
+++ qt/lesson_6/translations/TextEdit_en_EN.ts
@@ -0,0 +1,294 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <location filename="../mainwindow.cpp" line="167"/>
+        <source>Текстовый редактор</source>
+        <translation>Text editor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="25"/>
+        <source>Текущий путь:</source>
+        <translation>Current path:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="69"/>
+        <source>Файл</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="82"/>
+        <source>Помощь</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="89"/>
+        <source>Окно</source>
+        <translation>Windows</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="131"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="134"/>
+        <source>Сохранить файл</source>
+        <translation>Save file</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="139"/>
+        <location filename="../mainwindow.ui" line="142"/>
+        <source>Открыть</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="145"/>
+        <source>Открыть текстовый файл</source>
+        <translation>Open a text file</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="150"/>
+        <location filename="../mainwindow.cpp" line="127"/>
+        <source>Сохранить как...</source>
+        <translation>Save as...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="153"/>
+        <source>Сохранить файл под другим именем</source>
+        <translation>Save the file under a different name</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="158"/>
+        <source>Выход</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="161"/>
+        <source>Выход из приложения</source>
+        <translation>Application exit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="166"/>
+        <location filename="../mainwindow.cpp" line="103"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="169"/>
+        <source>Справка по программе</source>
+        <translation>Program note</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="172"/>
+        <source>Ctrl+C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="177"/>
+        <source>Открыть только для чтения</source>
+        <translation>Open for reading only</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="180"/>
+        <source>Открыть файл только для чтения</source>
+        <translation>Open a read-only file</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="185"/>
+        <location filename="../mainwindow.cpp" line="199"/>
+        <source>Настройки</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <source>Настройки программы</source>
+        <translation>Application settings</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="193"/>
+        <location filename="../mainwindow.cpp" line="290"/>
+        <source>Печать</source>
+        <translation>Print</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="196"/>
+        <source>Распечатать файл</source>
+        <translation>Print file</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="201"/>
+        <source>Свернуть все окна</source>
+        <translation>Collapse all windows</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="206"/>
+        <source>Восстановить все окна</source>
+        <translation>Restore all windows</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="211"/>
+        <source>Расположить каскадом</source>
+        <translation>Cascade</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="216"/>
+        <source>Расположить плиткой</source>
+        <translation>Tile</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="28"/>
+        <location filename="../mainwindow.cpp" line="137"/>
+        <location filename="../mainwindow.cpp" line="151"/>
+        <location filename="../mainwindow.cpp" line="179"/>
+        <location filename="../mainwindow.cpp" line="265"/>
+        <source> - Текстовый редактор</source>
+        <translation> - Text editor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="98"/>
+        <source>Файл справки отсутствует.</source>
+        <translation>There is no help file.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="155"/>
+        <source>Выберите текстовый файл</source>
+        <translation>Select a text file</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="168"/>
+        <source>Документ был изменен.
+Сохранить изменения?</source>
+        <translation>Tthe document has been changed.
+Save the changes?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="197"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.h" line="61"/>
+        <source>Новый файл</source>
+        <translation>New file</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../settings.ui" line="26"/>
+        <source>Язык программы</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="38"/>
+        <source>Русский</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="54"/>
+        <source>Английский</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="68"/>
+        <source>Горячие клавиши</source>
+        <translation>Hot keys</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="80"/>
+        <source>Открыть:</source>
+        <translation>Open:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="93"/>
+        <source>Сохранить:</source>
+        <translation>Save:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="106"/>
+        <source>Настройки:</source>
+        <translation>Settings:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="119"/>
+        <source>Справка:</source>
+        <translation>Help:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="132"/>
+        <source>Выход</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <source>none</source>
+        <translation type="vanished">none</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="155"/>
+        <location filename="../settings.ui" line="178"/>
+        <location filename="../settings.ui" line="191"/>
+        <location filename="../settings.ui" line="214"/>
+        <location filename="../settings.ui" line="237"/>
+        <location filename="../settings.ui" line="270"/>
+        <location filename="../settings.ui" line="296"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="283"/>
+        <source>Открыть для чтения:</source>
+        <translation>Open for reading:</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="309"/>
+        <source>Печать</source>
+        <translation>Print</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="333"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="346"/>
+        <source>Отмена</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="359"/>
+        <source>Тема оформления</source>
+        <translation>Design theme</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="371"/>
+        <source>Светлая</source>
+        <translation>Light</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="387"/>
+        <source>Темная</source>
+        <translation>Dark</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="66"/>
+        <location filename="../settings.cpp" line="68"/>
+        <location filename="../settings.cpp" line="70"/>
+        <location filename="../settings.cpp" line="72"/>
+        <location filename="../settings.cpp" line="74"/>
+        <location filename="../settings.cpp" line="76"/>
+        <location filename="../settings.cpp" line="78"/>
+        <location filename="../settings.cpp" line="109"/>
+        <source>Задайте клавишу...</source>
+        <translation>Set the key...</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="130"/>
+        <source>Настройки</source>
+        <translation>Settings</translation>
+    </message>
+</context>
+</TS>
