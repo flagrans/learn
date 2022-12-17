6df2c8b842f23d164ea2e7d7d046f5fb348d14e5
flagrans <flagrans@mail.ru>
2022-12-17 12:27:29 +0700

    UE Home work #2

---
+++ ue5/lesson_2/Source/TankZ/TankPawn.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "TankPawn.generated.h"
+
+class UStaticMeshComponent;
+UCLASS()
+class TANKZ_API ATankPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	    ATankPawn();
+        virtual void Tick(float DeltaTime) override;
+        void Move(float Value);
+        void MoveForward(float Value);
+        void MoveRight(float Value);
+
+protected:
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+		UStaticMeshComponent* BodyMesh;
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+        UStaticMeshComponent* TurretMesh;
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+        class UBoxComponent* BoxCollision;
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+        class USpringArmComponent* SpringArm;
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+        class UCameraComponent* Camera;
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+        float MoveSpeed = 100.0f;
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+        float RotationSpeed = 100.0f;
+
+        float targetForwardAxisValue = 0.0f;
+
+        float targetRightAxisValue = 0.0f;
+
+
+
+};
